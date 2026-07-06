d45bf59c22aaec1afc5168c927499c49bf590f23
Not-a-dude <honkhonkovic842@gmail.com>
2026-07-06 16:00:32 +0700

    build: bump version name/code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ val hasSigningConfig = listOf(
     "ZOMDROID_KEY_PASSWORD"
 ).all { localProperties[it] != null }
 
-val appVersionName = "1.4.0"
-val appVersionCode = 5
+val appVersionName = "1.4.1"
+val appVersionCode = 6
 
 extensions.configure<ApplicationExtension> {
     namespace = "com.zomdroid"
